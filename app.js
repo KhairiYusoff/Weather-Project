62f6d2157f7cdb36389b00f891781b2fc84b3ac3
Mohd Khairi Mohd Yusoff <102360814+KhairiYusoff@users.noreply.github.com>
2022-10-11 17:28:06 +0800

    Update App

--- app.js
+++ app.js
@@ -1,3 +1,5 @@
+
+
 const express = require("express");
 const https = require("https");
 const bodyParser = require("body-parser");
@@ -9,6 +11,9 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.get("/", function (req, res) {
     res.sendFile(__dirname + "/index.html")
 })
+
+//https://api.openweathermap.org/data/2.5/weather?q=london&appid=51036f1f5d0c0e0cfa3e1ec93dbba941&units=metric
+
 
 app.post("/", function (req, res) {
     const query = req.body.cityName;
